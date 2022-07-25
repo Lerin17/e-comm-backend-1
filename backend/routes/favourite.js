038c218925c3b12adfffc916f0d333d962d73b4e
Lerin17 <lerinowoade17@gmail.com>
2022-07-25 10:44:28 +0100

    first commit

---
+++ backend/routes/favourite.js
@@ -0,0 +1,78 @@
+
+
+const express = require('express')
+const router  = express.Router()
+
+const Favourite = require('../models/Favourite')
+const Product = require('../models/product')
+
+//add new product to favourites
+router.post('/:id', async(req, res)=>{
+    try{
+        const getProduct = await Product.findById(req.params.id)
+
+    const newfavouriteProduct = new Favourite({
+    name: getProduct.name,
+    price: getProduct.price,
+    description: getProduct.description,
+    type: getProduct.type,
+    color: getProduct.color,
+    image:getProduct.image,
+    imagearray:getProduct.imagearray,
+    alt:getProduct.alt,
+    productID:getProduct._id,
+    isNewArrival: req.body.isNewArrival
+    // username: req.body.username
+})
+
+        const favProduct = await newfavouriteProduct.save()
+        res.status(200).json(favProduct)
+    }catch(err){
+        res.status(500).json(err)
+    }
+  
+})
+
+//remove product from favourites
+router.delete('/:id', async(req, res)=>{
+    try{
+        await Product.findByIdAndDelete()
+       res.status(200).json("product has been deleted...")
+   }catch(err){
+       res.status(500).json(err)
+   }
+})
+
+
+module.exports = router
+// try{
+//     await Product.findByIdAndDelete()
+//    res.status(200).json("product has been deleted...")
+// }catch(err){
+//    res.status(500).json(err)
+// }
+
+
+// router.post('/',  verifyTokenandAdmin, async(req, res)=>{
+//     const newProduct = new Favourite({
+//         name: req.body.name,
+//         price: req.body.price,
+//         description: req.body.description,
+//         type: req.body.type,
+//         color: req.body.color,
+//         image:req.body.image,
+//         imagearray:req.body.imagearray,
+//         alt:req.body.alt,
+//         // ProductID: req.body.ProductID,
+//         isNewArrival: req.body.isNewArrival
+//         // username: req.body.username
+//     })
+
+//     try{
+//         const savedProduct = await newProduct.save()
+//         res.status(200).json(savedProduct)
+//     }
+//     catch(err){
+//         res.status(500).json(err)
+//     }
+// })
---
+++ backend/routes/verifyToken.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken")
+const router = require("./products")
+
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token
+    if(authHeader){
+        const token = authHeader
+        jwt.verify(token, process.env.JWT_SEC, (err, user)=>{
+           if(err) res.status(403).json("token is not valid")
+           req.user = user 
+        //    console.log(user)
+           next()
+        })
+    }else{
+        return res.status(401).json("you are not authenticated")
+    }
+}
+
+
+const verifyTokenandAuth = (req, res, next )=> {
+
+    verifyToken(req, res ,  ()=>{
+        // console.log(req.user)
+        if(req.user.id === req.params.id || req.user.Adminrole){
+            next()
+        }else{
+            req.status(403).json('You do not have clearance for this operation')
+        }
+    })
+}
+
+const verifyTokenandAdmin  = (req, res, next )=> {
+    // console.log(req)
+    verifyToken(req, res , ()=>{
+        // console.log(req.user)
+        if(req.user.isAdmin){
+          
+            next()
+        }else{
+            res.status(403).json('You do not have clearance for this admin  operation')
+        }
+    })
+}
+
+
+
+
+module.exports = {verifyToken, verifyTokenandAuth, verifyTokenandAdmin}
